6eedc092e2761fbd58128dc7400ec2a1bb61c2e0
Shiz <hi@shiz.me>
2014-07-28 22:54:12 +0200

    Fix incorrect exit guard in OpenSSL build script.
    It would invoke exit if the directory already existed (e.g. the first
    condition failed), which obviously isn't the intention. See also
    issue #1.

--- src/openssl-1.0.0l.sh
+++ src/openssl-1.0.0l.sh
@@ -4,7 +4,7 @@ rm -rf openssl-1.0.0l
 tar -xf openssl-1.0.0l.tar.gz || exit 1
 pushd openssl-1.0.0l >/dev/null
 
-([[ ! -d "${PREFIX}/share" ]] && mkdir "${PREFIX}/share") || exit 1
+[[ ! -d "${PREFIX}/share" ]] && (mkdir "${PREFIX}/share" || exit 1)
 ./Configure dist --prefix="${PREFIX}" --openssldir="${PREFIX}/share" || exit 1
 make || exit 1
 make install || exit 1
