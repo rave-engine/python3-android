be174896675b975ced84c2f78729f2cf57b1d72a
Shiz <hi@shiz.me>
2014-07-28 22:55:48 +0200

    Change order of find arguments for GNU findutils compatability.
    See also issue #1.

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 source ./env
 
-find "$BASE/src" -type d -mindepth 1 -exec rm -rf "{}" \;
+find "$BASE/src" -mindepth 1 -type d -exec rm -rf "{}" \;
 rm -rf "$ANDROID_PREFIX/"*
 rm -rf "$ANDROID_TOOL_PREFIX/"*
 rm -rf "$BASE/.built"
