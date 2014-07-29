8a58cb6f73118480654b6c158aeb7b6c5072426e
Shiz <hi@shiz.me>
2014-07-29 11:30:05 +0200

    Don't install OpenSSL docs.

--- src/openssl-1.0.0l.sh
+++ src/openssl-1.0.0l.sh
@@ -7,7 +7,7 @@ pushd openssl-1.0.0l >/dev/null
 [[ ! -d "${PREFIX}/share" ]] && (mkdir "${PREFIX}/share" || exit 1)
 ./Configure dist --prefix="${PREFIX}" --openssldir="${PREFIX}/share" || exit 1
 make || exit 1
-make install || exit 1
+make install_sw || exit 1
 
 popd >/dev/null
 popd >/dev/null
