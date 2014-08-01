afb201651aeae4288e319943edf65d31bbf5452c
Shiz <hi@shiz.me>
2014-08-01 16:25:05 +0200

    Split up package names and versions.

--- build.sh
+++ build.sh
@@ -11,9 +11,10 @@ prepare_ndk() {
 
 build() {
    what=$1
+   version=$2
 
-   if [[ ! -f "${BASE}/.built/${what}" ]]; then
-       (/usr/bin/env bash --noprofile "${BASE}/_build_single.sh" "src/${what}.sh" && touch "${BASE}/.built/${what}") || exit 1
+   if [[ ! -f "${BASE}/.built/${what}-${version}" ]]; then
+       (/usr/bin/env bash --noprofile "${BASE}/_build_single.sh" "src/${what}-${version}.sh" && touch "${BASE}/.built/${what}-${version}") || exit 1
    fi
 }
 
@@ -22,9 +23,9 @@ build() {
 [[ ! -d "${BASE}/.built" ]] && (mkdir "${BASE}/.built" || exit 1)
 
 prepare_ndk
-build bzip2-1.0.6
-build xz-5.0.5
-build openssl-1.0.0l
-build Python-3.3.3
+build bzip2   1.0.6
+build xz      5.0.5
+build openssl 1.0.0l
+build Python  3.3.3
 
 popd >/dev/null
