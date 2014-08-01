2c28aa34f851541dd0ac22638a4117a1313d55c8
Shiz <hi@shiz.me>
2014-08-02 01:33:30 +0200

    Improve clean script.

--- clean.sh
+++ clean.sh
@@ -1,7 +1,6 @@
 source ./env
 
-find "$BASE/src" -mindepth 1 -type d -exec rm -rf "{}" \;
-rm -rf "$ANDROID_PREFIX/"*
-rm -rf "$ANDROID_TOOL_PREFIX/"*
+find "$BASE/src" -depth 1 -type d -exec rm -rf "{}" \;
+rm -rf "$ANDROID_PREFIX"
+rm -rf "$ANDROID_TOOL_PREFIX"
 rm -rf "$BASE/.built"
-rm -rf "$BASE/src/"*.tar.*
