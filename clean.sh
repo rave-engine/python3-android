4328dfd02ba61049fe97c32caf410af382af86fa
Shiz <hi@shiz.me>
2014-08-02 02:47:00 +0200

    Fix broken find invocation in clean.sh.

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 source ./env
 
-find "$BASE/src" -depth 1 -type d -exec rm -rf "{}" \;
+find "$BASE/src" -mindepth 1 -maxdepth 1 -type d -exec rm -rf "{}" \;
 rm -rf "$ANDROID_PREFIX"
 rm -rf "$ANDROID_TOOL_PREFIX"
 rm -rf "$BASE/.built"
