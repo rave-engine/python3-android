c8c0b0944e4a3e5a5763831b5ff879c3cd58fa91
Shiz <hi@shiz.me>
2014-08-04 23:28:10 +0200

    Fix symlink locations for ncurses include files.

--- mk/ncurses/5.9/build.sh
+++ mk/ncurses/5.9/build.sh
@@ -9,8 +9,8 @@ patch -p1 < "$BASE/mk/ncurses/5.9/ncurses-5.9-android-locale-fixes.patch"
 make || exit 1
 make install || exit 1
 # Fix symlinks for Python _curses and _curses_panel extensions.
-ln -s ncursesw/curses.h "${PREFIX}/curses.h"
-ln -s ncursesw/panel.h "${PREFIX}/panel.h"
+ln -s ncursesw/curses.h "${PREFIX}/include/curses.h"
+ln -s ncursesw/panel.h "${PREFIX}/include/panel.h"
 
 popd >/dev/null
 popd >/dev/null
