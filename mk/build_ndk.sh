2cb04c395f81c739e8c63b447904f12d7bd673ff
Shiz <hi@shiz.me>
2014-08-04 18:50:34 +0200

    Fix architecture detection for NDK.

--- mk/build_ndk.sh
+++ mk/build_ndk.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 source ./env
 
+# Stolen from https://github.com/rust-lang/rust/blob/2e2f53fad/configure#L345.
+case $(uname -m) in
+  i386 | i486 | i686 | i786 | x86)
+      NDK_ARCH="x86"
+      ;;
+  x86-64 | x86_64 | x64 | amd64)
+      NDK_ARCH="x86_64"
+      ;;
+  *)
+      echo "Unknown architecture: $(uname -m)."
+      exit 1
+      ;;
+esac
+
 [[ ! -d "${ANDROID_PREFIX}/.built-${BUILD_IDENTIFIER}" ]] && (mkdir -p "${ANDROID_PREFIX}/.built-${BUILD_IDENTIFIER}" || exit 1)
 [[ ! -d "${ANDROID_TOOL_PREFIX}/${BUILD_IDENTIFIER}" ]] && (mkdir -p "${ANDROID_TOOL_PREFIX}/${BUILD_IDENTIFIER}" || exit 1)
-[[ ! -d "${BASE}/sdk/${NDK_REL}" ]] && (tar -xf "$BASE/sdk/android-ndk-r${NDK_REV}-$(uname -s | tr '[A-Z]' '[a-z]')-$(uname -m).tar.bz2" -C "$BASE/sdk" || exit 1)
+[[ ! -d "${BASE}/sdk/${NDK_REL}" ]] && (tar -xf "$BASE/sdk/android-ndk-r${NDK_REV}-$(uname -s | tr '[A-Z]' '[a-z]')-${NDK_ARCH}.tar.bz2" -C "$BASE/sdk" || exit 1)
 
 if [[ ! -f "${ANDROID_PREFIX}/.built-ndk-${BUILD_IDENTIFIER}" ]]; then
     ("${BASE}/sdk/${NDK_REL}/build/tools/make-standalone-toolchain.sh" --platform="android-${ANDROID_API_LEVEL}" --install-dir="${ANDROID_TOOL_PREFIX}/${BUILD_IDENTIFIER}" --toolchain="${ANDROID_TOOLCHAIN}" &&\
--- mk/ndk_source.sh
+++ mk/ndk_source.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
-
 source ./env
-echo http://dl.google.com/android/ndk/android-ndk-r${NDK_REV}-$(uname -s | tr '[A-Z]' '[a-z'])-$(uname -m).tar.bz2
+
+# Stolen from https://github.com/rust-lang/rust/blob/2e2f53fad/configure#L345.
+case $(uname -m) in
+  i386 | i486 | i686 | i786 | x86)
+      NDK_ARCH="x86"
+      ;;
+  x86-64 | x86_64 | x64 | amd64)
+      NDK_ARCH="x86_64"
+      ;;
+  *)
+      echo "Unknown architecture: $(uname -m)."
+      exit 1
+      ;;
+esac
+echo http://dl.google.com/android/ndk/android-ndk-r${NDK_REV}-$(uname -s | tr '[A-Z]' '[a-z'])-${NDK_ARCH}.tar.bz2
