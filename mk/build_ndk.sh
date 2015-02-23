7643cfdf78cfee7d404e61a2e1a3bd66cf7ee51d
GRRedWings <spratke@gmail.com>
2015-02-23 16:36:17 -0500

    Found a problem with the way we extract and move the NDK. Second try, but I think I figured out my lack of understanding of sh files, and tested to make sure this one works.

--- mk/build_ndk.sh
+++ mk/build_ndk.sh
@@ -18,10 +18,11 @@ esac
 [[ ! -d "${ANDROID_PREFIX}/.built-${BUILD_IDENTIFIER}" ]] && (mkdir -p "${ANDROID_PREFIX}/.built-${BUILD_IDENTIFIER}" || exit 1)
 [[ ! -d "${ANDROID_TOOL_PREFIX}/${BUILD_IDENTIFIER}" ]] && (mkdir -p "${ANDROID_TOOL_PREFIX}/${BUILD_IDENTIFIER}" || exit 1)
 
-[[ ! -d "$BASE/sdk/android-ndk-r${NDK_REV}-$(uname -s | tr '[A-Z]' '[a-z]')-${NDK_ARCH}.bin" ]] && (chmod a+x "$BASE/sdk/android-ndk-r${NDK_REV}-$(uname -s | tr '[A-Z]' '[a-z]')-${NDK_ARCH}.bin" || exit 1)
-[[ ! -d "$BASE/sdk/android-ndk-r${NDK_REV}-$(uname -s | tr '[A-Z]' '[a-z]')-${NDK_ARCH}.bin" ]] && ("$BASE/sdk/android-ndk-r${NDK_REV}-$(uname -s | tr '[A-Z]' '[a-z]')-${NDK_ARCH}.bin" || exit 1)
 
-[[ ! -d "$BASE/android-ndk-r${NDK_REV}" ]] && ("mv -f $BASE/android-ndk-r${NDK_REV} $BASE/sdk/android-ndk-r${NDK_REV}" || exit 1)
+[[ -f "${BASE}/sdk/android-ndk-r${NDK_REV}-$(uname -s | tr '[A-Z]' '[a-z]')-${NDK_ARCH}.bin" ]] && (chmod a+x "${BASE}/sdk/android-ndk-r${NDK_REV}-$(uname -s | tr '[A-Z]' '[a-z]')-${NDK_ARCH}.bin" || exit 1)
+[[ -f "${BASE}/sdk/android-ndk-r${NDK_REV}-$(uname -s | tr '[A-Z]' '[a-z]')-${NDK_ARCH}.bin" ]] && ("${BASE}/sdk/android-ndk-r${NDK_REV}-$(uname -s | tr '[A-Z]' '[a-z]')-${NDK_ARCH}.bin" || exit 1)
+
+[[ -d "${BASE}/android-ndk-r${NDK_REV}" ]] && (mv -f "${BASE}/android-ndk-r${NDK_REV}" "${BASE}/sdk/android-ndk-r${NDK_REV}" || exit 1)
 
 if [[ ! -f "${ANDROID_PREFIX}/.built-ndk-${BUILD_IDENTIFIER}" ]]; then
     ("${BASE}/sdk/${NDK_REL}/build/tools/make-standalone-toolchain.sh" --platform="android-${ANDROID_API_LEVEL}" --install-dir="${ANDROID_TOOL_PREFIX}/${BUILD_IDENTIFIER}" --toolchain="${ANDROID_TOOLCHAIN}" &&\
