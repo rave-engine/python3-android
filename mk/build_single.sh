624ea0f4b7d159767bdd077bf2124fcf31db6238
GRRedWings <spratke@gmail.com>
2015-06-26 15:33:50 -0400

    Change from cFlags to LDFlags,
    I believe that we need this for link, which I'm kinda guessing is the ldflags, not the cflags.

--- mk/build_single.sh
+++ mk/build_single.sh
@@ -15,10 +15,10 @@ export NDK_PLATFORM="android-${NDK_REV}"
 export SDK_PLATFORM="android-${SDK_REV}"
 export cross="${ANDROID_TARGET}-"
 
-export CFLAGS="--sysroot ${TOOL_PREFIX}/sysroot -I${PREFIX}/include -I${TOOL_PREFIX}/include -DANDROID -mandroid ${CFLAGS_EXTRA} -Wl,-soname,lib$13.4m.so"
+export CFLAGS="--sysroot ${TOOL_PREFIX}/sysroot -I${PREFIX}/include -I${TOOL_PREFIX}/include -DANDROID -mandroid ${CFLAGS_EXTRA}"
 export CPPFLAGS="${CFLAGS} ${CPPFLAGS_EXTRA}"
 export CXXFLAGS="${CFLAGS} ${CXXFLAGS_EXTRA}"
-export LDFLAGS="--sysroot ${TOOL_PREFIX}/sysroot -L${PREFIX}/lib -L${TOOL_PREFIX}/lib ${LDFLAGS_EXTRA}"
+export LDFLAGS="--sysroot ${TOOL_PREFIX}/sysroot -L${PREFIX}/lib -L${TOOL_PREFIX}/lib ${LDFLAGS_EXTRA} -Wl,-soname,lib$13.4m.so"
 
 export CC="${ANDROID_TARGET}-gcc"
 export CXX="${ANDROID_TARGET}-g++"
