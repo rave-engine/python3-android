71bd8c0cfed672ea266a62d23f35f878cf98238f
Yen Chi Hsuan <yan12125@gmail.com>
2015-11-26 19:19:29 +0800

    Updated patch for libm linking

--- mk/python/hg/build.sh
+++ mk/python/hg/build.sh
@@ -21,6 +21,7 @@ patch -p1  < "${FILESDIR}/${PACKAGE}-android-locale.patch" || exit 1
 patch -Ep1 < "${FILESDIR}/${PACKAGE}-android-libmpdec.patch" || exit 1
 [[ "${NDK_REV}" != 10* ]] && (patch -p1 < "${FILESDIR}/${PACKAGE}-android-missing-getdents64-definition.patch" || exit 1)
 patch -p1  < "${FILESDIR}/${PACKAGE}-android-misc.patch" || exit 1
+patch -p1  < "${FILESDIR}/${PACKAGE}-modules-link-libm.patch" || exit 1
 
 ./configure CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" CONFIG_SITE=config.site --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --disable-ipv6 --enable-shared --without-ensurepip || exit 1
 make CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" HOSTPGEN="$(pwd)/Parser/hostpgen" || exit 1
