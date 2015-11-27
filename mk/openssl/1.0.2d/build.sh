724979eb5b9e98afdcf5355617a772dcdd6e111a
Yen Chi Hsuan <yan12125@gmail.com>
2015-11-28 03:50:19 +0800

    Build shared OpenSSL libraries

--- mk/openssl/1.0.2d/build.sh
+++ mk/openssl/1.0.2d/build.sh
@@ -6,9 +6,15 @@ pushd "${PACKAGE}" >/dev/null
 
 [[ ! -d "${PREFIX}/share" ]] && (mkdir "${PREFIX}/share" || exit 1)
 patch -p1 < "${FILESDIR}/${PACKAGE}-android-ndk-target.patch" || exit 1
-./Configure android-ndk --prefix="${PREFIX}" --openssldir="${PREFIX}/share" || exit 1
-make || exit 1
-make install_sw || exit 1
+patch -p1 < "${FILESDIR}/${PACKAGE}-disable-libversion.patch" || exit 1
+./Configure android-ndk --prefix="${PREFIX}" --openssldir="${PREFIX}/share" shared || exit 1
+make DISABLE_LIBVERSION=1 || exit 1
+make DISABLE_LIBVERSION=1 install_sw || exit 1
+
+OPENSSL_VER=1.0.0
+for comp in ssl crypto ; do
+    rm ${PREFIX}/lib/lib${comp}.a || exit 1
+done
 
 # Remove binaries from premises.
 rm -f "${PREFIX}/bin/"{openssl,c_rehash} || exit 1
