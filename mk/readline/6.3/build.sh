c82ef87d214e91a72932f091517b709c3a9f9f44
Yen Chi Hsuan <yan12125@gmail.com>
2015-12-03 19:46:00 +0800

    readline-6.3: Build unversioned so

--- mk/readline/6.3/build.sh
+++ mk/readline/6.3/build.sh
@@ -4,8 +4,10 @@ rm -rf "${PACKAGE}"
 tar -xf "${PACKAGE}.tar.gz" || exit 1
 pushd "${PACKAGE}" >/dev/null
 
+patch -p1 < "${FILESDIR}/${PACKAGE}-build-unversioned-so.patch" || exit 1
+
 autoreconf -i
-./configure --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --disable-shared || exit 1
+./configure --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --disable-static || exit 1
 make || exit 1
 make install || exit 1
 
