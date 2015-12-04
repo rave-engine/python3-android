8bd7cf57bf928ab7e9c3c604f86d7186d187ddca
Yen Chi Hsuan <yan12125@gmail.com>
2015-12-04 15:48:40 +0800

    ncurses-5.9: Build shared libraries without versioning

--- mk/ncurses/5.9/build.sh
+++ mk/ncurses/5.9/build.sh
@@ -5,12 +5,14 @@ tar -xf "${PACKAGE}.tar.gz" || exit 1
 pushd "${PACKAGE}" >/dev/null
 
 patch -p1 < "${FILESDIR}/${PACKAGE}-android-locale-fixes.patch"
-./configure --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --without-ada --without-manpages --without-progs --without-tests --with-termlib --enable-termcap --enable-widec --disable-database --disable-home-terminfo || exit 1
+patch -p1 < "${FILESDIR}/${PACKAGE}-fix-bash-syntax-error.patch"
+./configure --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --without-ada --without-manpages --without-progs --without-tests --without-termlib --enable-termcap --enable-widec --disable-database --disable-home-terminfo --with-shared --without-normal --without-debug --without-shlib-version || exit 1
 make || exit 1
 make install || exit 1
 # Fix symlinks for Python _curses and _curses_panel extensions.
 ln -s ncursesw/curses.h "${PREFIX}/include/curses.h" || exit 1
 ln -s ncursesw/panel.h "${PREFIX}/include/panel.h" || exit 1
+rm ${PREFIX}/lib/libncurses++w.a || exit 1
 
 popd >/dev/null
 popd >/dev/null
