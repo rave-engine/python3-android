6754f88c5435d50ae74574c6f3b8b31e82d11798
Yen Chi Hsuan <yan12125@gmail.com>
2015-12-04 17:07:04 +0800

    ncurses-5.9: Disable C++ bindings
    Currently cross-compiling C++ shared libraries is not working

--- mk/ncurses/5.9/build.sh
+++ mk/ncurses/5.9/build.sh
@@ -6,13 +6,12 @@ pushd "${PACKAGE}" >/dev/null
 
 patch -p1 < "${FILESDIR}/${PACKAGE}-android-locale-fixes.patch"
 patch -p1 < "${FILESDIR}/${PACKAGE}-fix-bash-syntax-error.patch"
-./configure --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --without-ada --without-manpages --without-progs --without-tests --without-termlib --enable-termcap --enable-widec --disable-database --disable-home-terminfo --with-shared --without-normal --without-debug --without-shlib-version || exit 1
+./configure --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --without-ada --without-manpages --without-progs --without-tests --without-termlib --enable-termcap --enable-widec --disable-database --disable-home-terminfo --with-shared --without-normal --without-debug --without-shlib-version --without-cxx-binding || exit 1
 make || exit 1
 make install || exit 1
 # Fix symlinks for Python _curses and _curses_panel extensions.
 ln -s ncursesw/curses.h "${PREFIX}/include/curses.h" || exit 1
 ln -s ncursesw/panel.h "${PREFIX}/include/panel.h" || exit 1
-rm ${PREFIX}/lib/libncurses++w.a || exit 1
 
 popd >/dev/null
 popd >/dev/null
