dec923d298db1fbc32e1d74ab6f955073ef565d2
Yen Chi Hsuan <yan12125@gmail.com>
2016-03-12 21:27:19 +0800

    openssl-1.0.2g: Build static libraries
    My ASUS Zenfone 2 has its own lib{crypto,ssl}.so, causing conflicts

--- mk/openssl/1.0.2g/build.sh
+++ mk/openssl/1.0.2g/build.sh
@@ -7,13 +7,9 @@ pushd "${PACKAGE}" >/dev/null
 [[ ! -d "${PREFIX}/share" ]] && (mkdir "${PREFIX}/share" || exit 1)
 patch -p1 < "${FILESDIR}/${PACKAGE}-android-ndk-target.patch" || exit 1
 patch -p1 < "${FILESDIR}/${PACKAGE}-disable-libversion.patch" || exit 1
-./Configure android-ndk --prefix="${PREFIX}" --openssldir="${PREFIX}/share" shared || exit 1
+./Configure android-ndk --prefix="${PREFIX}" --openssldir="${PREFIX}/share" || exit 1
 make DISABLE_LIBVERSION=1 || exit 1
 make DISABLE_LIBVERSION=1 install_sw || exit 1
-
-for comp in ssl crypto ; do
-    rm ${PREFIX}/lib/lib${comp}.a || exit 1
-done
 
 # Remove binaries from premises.
 rm -f "${PREFIX}/bin/"{openssl,c_rehash} || exit 1
