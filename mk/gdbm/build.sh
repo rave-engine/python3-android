12c3afb3dcda7ccbe0d03b159941552d1977302f
Yen Chi Hsuan <yan12125@gmail.com>
2016-03-21 16:16:46 +0800

    gdbm-1.11: Disable .so versioning
    Symlinks are causing EACCES errors on my phone

--- mk/gdbm/build.sh
+++ mk/gdbm/build.sh
@@ -5,6 +5,8 @@ tar -xf "${PACKAGE}.tar.gz" || exit 1
 pushd "${PACKAGE}" >/dev/null
 
 patch -p1 < "${FILESDIR}/android-winsize.patch"
+patch -p1 < "${FILESDIR}/disable-so-versioning.patch"
+autoreconf --install --verbose --symlink --force
 ./configure --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --enable-libgdbm-compat || exit 1
 make || exit 1
 make install || exit 1
