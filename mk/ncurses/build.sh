97e2299767be40c21dffccf27ad29566f8beec67
Yen Chi Hsuan <yan12125@gmail.com>
2016-03-21 17:09:13 +0800

    ncurses-5.9: Do not install curses.h
    Android doesn't like symlinks

--- mk/ncurses/build.sh
+++ mk/ncurses/build.sh
@@ -5,7 +5,26 @@ tar -xf "${PACKAGE}.tar.gz" || exit 1
 pushd "${PACKAGE}" >/dev/null
 
 patch -p1 < "${FILESDIR}/fix-bash-syntax-error.patch"
-./configure --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --without-ada --without-manpages --without-progs --without-tests --without-termlib --enable-termcap --enable-widec --disable-database --disable-home-terminfo --with-shared --without-normal --without-debug --without-shlib-version --without-cxx-binding --enable-overwrite || exit 1
+./configure \
+    --prefix="${PREFIX}" \
+    --host="${TARGET}" \
+    --build="${HOST}" \
+    --without-ada \
+    --without-manpages \
+    --without-progs \
+    --without-tests \
+    --without-termlib \
+    --enable-termcap \
+    --enable-widec \
+    --disable-database \
+    --disable-home-terminfo \
+    --with-shared \
+    --without-normal \
+    --without-debug \
+    --without-shlib-version \
+    --without-cxx-binding \
+    --enable-overwrite \
+    --without-curses-h || exit 1
 make || exit 1
 make install || exit 1
 
