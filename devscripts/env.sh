ca54cf481e8ab1ac82754f5af3bb62211c4ef562
Yen Chi Hsuan <yan12125@gmail.com>
2016-03-26 02:38:24 +0800

    Don't put invalid paths to LD_LIBRARY_PATH
    Android 6.0 complains about it

--- devscripts/env.sh
+++ devscripts/env.sh
@@ -1,5 +1,8 @@
 PYTHON3_ROOT=/data/local/tmp/python3
 
 export PATH=$PATH:$PYTHON3_ROOT/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PYTHON3_ROOT/lib
+if [ ! -z "$LD_LIBRARY_PATH" ] ; then
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:"
+fi
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH$PYTHON3_ROOT/lib"
 export SSL_CERT_DIR=$PYTHON3_ROOT/share/certs
