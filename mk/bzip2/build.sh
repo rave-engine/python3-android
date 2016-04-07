5775455588c2380e364201833a7d08ef2263d852
Yen Chi Hsuan <yan12125@gmail.com>
2016-04-07 14:49:14 +0800

    bzip2-1.0.6: Build binaries as well

--- mk/bzip2/build.sh
+++ mk/bzip2/build.sh
@@ -5,9 +5,9 @@ tar -xf "${PACKAGE}.tar.gz" || exit 1
 pushd "${PACKAGE}" >/dev/null
 
 patch -p1 < "${FILESDIR}/makefile-env.patch" || exit 1
-make clean || exit 1
-make libbz2.a || exit 1
-make install_libbz2.a || exit 1
+CFLAGS="$CPPFLAGS $CFLAGS" make clean || exit 1
+CFLAGS="$CPPFLAGS $CFLAGS" make libbz2.a bzip2 bzip2recover || exit 1
+CFLAGS="$CPPFLAGS $CFLAGS" make install || exit 1
 
 popd >/dev/null
 popd >/dev/null
