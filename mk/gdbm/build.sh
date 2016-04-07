ae6485333865b119c50185645a1bbb161173e278
Yen Chi Hsuan <yan12125@gmail.com>
2016-04-07 14:48:24 +0800

    gdbm-1.11: Fix missing function from gdbmtool.h

--- mk/gdbm/build.sh
+++ mk/gdbm/build.sh
@@ -4,8 +4,8 @@ rm -rf "${PACKAGE}"
 tar -xf "${PACKAGE}.tar.gz" || exit 1
 pushd "${PACKAGE}" >/dev/null
 
-patch -p1 < "${FILESDIR}/android-winsize.patch"
 patch -p1 < "${FILESDIR}/disable-so-versioning.patch"
+patch -p1 < "${FILESDIR}/gdbmtool-h-missing-function.patch"
 autoreconf --install --verbose --symlink --force
 ./configure --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --enable-libgdbm-compat || exit 1
 make || exit 1
