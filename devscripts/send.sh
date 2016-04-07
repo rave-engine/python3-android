686d6f8425d4bd39fe398be10bdfbc66c35aaae7
Yen Chi Hsuan <yan12125@gmail.com>
2016-04-07 14:55:45 +0800

    devscripts/send.sh: Use plain tar as some ROMs are missing gzip

--- devscripts/send.sh
+++ devscripts/send.sh
@@ -10,10 +10,10 @@ cd build
 mkdir -p $NAME/tools
 cp ../devscripts/{c_rehash.py,env.sh} $NAME/tools
 
-tar zcf ${NAME}.tar.gz $NAME
-adb shell rm /sdcard/${NAME}.tar.gz
-adb push ${NAME}.tar.gz /sdcard/${NAME}.tar.gz
+tar cf ${NAME}.tar $NAME
+adb shell rm /sdcard/${NAME}.tar
+adb push ${NAME}.tar /sdcard/${NAME}.tar
 
 adb shell rm -rf $TMP/python3
-adb shell tar zxvf /sdcard/${NAME}.tar.gz -C $TMP
+adb shell tar xvf /sdcard/${NAME}.tar -C $TMP
 adb shell mv $TMP/${NAME} $TMP/python3
