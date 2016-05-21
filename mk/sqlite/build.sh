404cc17ee07675d9929dc31cc9609567d21dc07e
Yen Chi Hsuan <yan12125@gmail.com>
2016-05-21 23:20:57 +0800

    sqlite-3.12.2: Bump

--- mk/sqlite/build.sh
+++ mk/sqlite/build.sh
@@ -1,8 +1,8 @@
 pushd src >/dev/null
 
-rm -rf "${NAME}-autoconf-3081002"
-tar -xf "${NAME}-autoconf-3081002.tar.gz" || exit 1
-pushd "${NAME}-autoconf-3081002" >/dev/null
+rm -rf "${NAME}-autoconf-3120200"
+tar -xf "${NAME}-autoconf-3120200.tar.gz" || exit 1
+pushd "${NAME}-autoconf-3120200" >/dev/null
 
 ./configure --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --disable-shared || exit 1
 make || exit 1
