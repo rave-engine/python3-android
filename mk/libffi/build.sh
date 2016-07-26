1daebca128e9ca1a9a39ef143ded7090cfa71cc9
Yen Chi Hsuan <yan12125@gmail.com>
2016-07-26 18:04:08 +0800

    libffi-git: Apply libffi/libffi#240 for closures on Android

--- mk/libffi/build.sh
+++ mk/libffi/build.sh
@@ -3,6 +3,8 @@ pushd src/libffi || exit 1
 git checkout .
 git clean -dfx
 
+patch -p1 < "${FILESDIR}/libffi-pr240-modded.patch" || exit 1
+
 ./autogen.sh
 
 ./configure --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" || exit 1
