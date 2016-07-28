3501d8bd9c48ea5034ae77751d45297dbd33cce1
Yen Chi Hsuan <yan12125@gmail.com>
2016-07-28 11:54:02 +0800

    Support multiple sources

--- mk/get_source.sh
+++ mk/get_source.sh
@@ -3,29 +3,30 @@ source_manifest="mk/$1/sources.txt"
 if [ ! -e "$source_manifest" ] ; then
     exit
 fi
-source_file=$(cat "$source_manifest")
 src_prefix="src/"
 
-if [[ "$source_file" == hg+* || "$source_file" == git+* ]] ; then
-    pushd "$src_prefix"
-    dest=$(basename "$source_file")
-    if [[ "$source_file" == hg+* ]] ; then
-        if [ -d "$dest" ] ; then
-            cd $dest
-            hg pull -u
+while read source_file ; do
+    if [[ "$source_file" == hg+* || "$source_file" == git+* ]] ; then
+        pushd "$src_prefix"
+        dest=$(basename "$source_file")
+        if [[ "$source_file" == hg+* ]] ; then
+            if [ -d "$dest" ] ; then
+                cd $dest
+                hg pull -u
+            else
+                hg clone ${source_file:3} "$dest"
+            fi
         else
-            hg clone ${source_file:3} "$dest"
+            if [ -d "$dest" ] ; then
+                cd $dest
+                git fetch --tags origin
+                git merge origin/master
+            else
+                git clone ${source_file:4} "$dest"
+            fi
         fi
+        popd
     else
-        if [ -d "$dest" ] ; then
-            cd $dest
-            git fetch --tags origin
-            git merge origin/master
-        else
-            git clone ${source_file:4} "$dest"
-        fi
+        wget -N -P "$src_prefix" "$source_file"
     fi
-    popd
-else
-    wget -N -P "$src_prefix" "$source_file"
-fi
+done < "$source_manifest"
