ed8de2b41aed2822826f6b16bd7453454412b9ce
Yen Chi Hsuan <yan12125@gmail.com>
2016-07-28 11:54:23 +0800

    readline-6.3: Apply readline patches up to 6.3.008

--- mk/readline/build.sh
+++ mk/readline/build.sh
@@ -4,6 +4,9 @@ rm -rf "${PACKAGE}"
 tar -xf "${PACKAGE}.tar.gz" || exit 1
 pushd "${PACKAGE}" >/dev/null
 
+for i in {001..008} ; do
+    patch -p0 < "${BASE}/src/readline63-$i" || exit 1
+done
 patch -p1 < "${FILESDIR}/build-unversioned-so.patch" || exit 1
 
 autoreconf -i
