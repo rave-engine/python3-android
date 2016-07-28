f555cac0290fa3bd5c7a1f3745042e331cce8822
Yen Chi Hsuan <yan12125@gmail.com>
2016-07-29 04:06:22 +0800

    readline-6.3: Fix build

--- mk/readline/build.sh
+++ mk/readline/build.sh
@@ -3,7 +3,12 @@ for i in {001..008} ; do
 done
 patch -p1 < "${FILESDIR}/build-unversioned-so.patch"
 
-autoreconf -i
-./configure --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --disable-static
+# See the wcwidth() test in aclocal.m4. Tested on Android 6.0 and it's broken
+./configure \
+    bash_cv_wcwidth_broken=yes \
+    --prefix="${PREFIX}" \
+    --host="${TARGET}" \
+    --build="${HOST}" \
+    --disable-static
 make
 make install
