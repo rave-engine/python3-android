62587155691d032d14ef26de9d3349e5034a34e2
Yen Chi Hsuan <yan12125@gmail.com>
2016-07-29 18:21:31 +0800

    gdbm-1.12: Disable static libraries

--- mk/gdbm/build.sh
+++ mk/gdbm/build.sh
@@ -1,5 +1,10 @@
 patch -p1 < "${FILESDIR}/disable-so-versioning.patch"
 autoreconf --install --verbose --symlink --force
-./configure --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --enable-libgdbm-compat
+./configure \
+    --prefix="${PREFIX}" \
+    --host="${TARGET}" \
+    --build="${HOST}" \
+    --enable-libgdbm-compat \
+    --disable-static
 make
 make install
