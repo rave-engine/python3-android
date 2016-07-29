1259d69dc6c602d6599c66d8306fc7d9fe7be9db
Yen Chi Hsuan <yan12125@gmail.com>
2016-07-30 03:10:40 +0800

    bzip2: Build shared libraries

--- mk/bzip2/build.sh
+++ mk/bzip2/build.sh
@@ -1,4 +1,11 @@
-patch -p1 < "${FILESDIR}/makefile-env.patch"
-CFLAGS="$CPPFLAGS $CFLAGS" make clean
-CFLAGS="$CPPFLAGS $CFLAGS" make libbz2.a bzip2 bzip2recover
-CFLAGS="$CPPFLAGS $CFLAGS" make install
+export CFLAGS="$CPPFLAGS $CFLAGS" 
+
+patch -p1 < "${FILESDIR}/makefiles.patch"
+
+make clean
+make bzip2recover
+make -f Makefile-libbz2_so
+
+make install
+cp -f bzip2-shared "${PREFIX}/bin/bzip2"
+cp -f libbz2.so "${PREFIX}/lib/libbz2.so"
