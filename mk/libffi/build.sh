3ab56e7b06696b2fc54017b6fa12f8dbd20dcf0d
Yen Chi Hsuan <yan12125@gmail.com>
2016-07-29 18:22:02 +0800

    libffi-git: Disable static libraries

--- mk/libffi/build.sh
+++ mk/libffi/build.sh
@@ -2,6 +2,10 @@ patch -p1 < "${FILESDIR}/libffi-pr240-modded.patch"
 
 ./autogen.sh
 
-./configure --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}"
+./configure \
+    --prefix="${PREFIX}" \
+    --host="${TARGET}" \
+    --build="${HOST}" \
+    --disable-static
 make
 make install
