b767dd6c246de75657e12039d6f380e8e2fb5825
Yen Chi Hsuan <yan12125@gmail.com>
2016-07-30 12:16:45 +0800

    Add default paths to c_rehash.py

--- devscripts/c_rehash.py
+++ devscripts/c_rehash.py
@@ -1,19 +1,24 @@
 import os
-import os.path
+from os.path import abspath, dirname, exists, join
 import subprocess
 import sys
 
-SSL_CERT_SRC = sys.argv[1]
-SSL_CERT_DST = sys.argv[2]
+ssl_cert_src = '/system/etc/security/cacerts'
+# This file is installed at $PREFIX/tools/
+ssl_cert_dst = join(dirname(dirname(abspath(__file__))), 'share', 'certs')
 
-for cert in os.listdir(SSL_CERT_SRC):
-    old_path = os.path.join(SSL_CERT_SRC, cert)
+if len(sys.argv) == 3:
+    ssl_cert_src = sys.argv[1]
+    ssl_cert_dst = sys.argv[2]
+
+for cert in os.listdir(ssl_cert_src):
+    old_path = join(ssl_cert_src, cert)
     new_hash = subprocess.check_output([
         'openssl', 'x509', '-noout', '-hash', '-in', old_path
     ]).strip().decode('ascii')
     for index in range(10):
-        new_path = os.path.join(SSL_CERT_DST, '%s.%s' % (new_hash, index))
-        if not os.path.exists(new_path):
+        new_path = join(ssl_cert_dst, '%s.%s' % (new_hash, index))
+        if not exists(new_path):
             break
     print('%s => %s' % (old_path, new_path))
     os.symlink(old_path, new_path)
