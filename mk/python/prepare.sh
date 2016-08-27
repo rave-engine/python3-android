72424492abbdee2e620a7d6b1d391f394cc2c311
Yen Chi Hsuan <yan12125@gmail.com>
2016-08-27 21:52:20 +0800

    python: Bump to a new patch for _ssl

--- mk/python/prepare.sh
+++ mk/python/prepare.sh
@@ -1,7 +1,7 @@
 patch -p1 < "${FILESDIR}/python-misc.patch"
 patch -p1 < "${FILESDIR}/android-misc.patch"
 patch -p1 < "${FILESDIR}/check-crypt.patch"
-patch -p1 < "${BASE}/src/Port-Python-s-SSL-module-to-OpenSSL-1.1.0-2.patch"
+patch -p1 < "${BASE}/src/Port-Python-s-SSL-module-to-OpenSSL-1.1.0-3.patch"
 
 rm -rf Modules/_ctypes/{darwin,libffi}*
 
