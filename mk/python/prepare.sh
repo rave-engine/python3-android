6efaa1e74afb3bcf079206c701e1736492c19d71
Yen Chi Hsuan <yan12125@gmail.com>
2016-09-05 12:36:03 +0800

    python: Bump to the latest OpenSSL 1.1.0 patch

--- mk/python/prepare.sh
+++ mk/python/prepare.sh
@@ -2,7 +2,7 @@ patch -p1 < "${FILESDIR}/cross-compile.patch"
 patch -p1 < "${FILESDIR}/python-misc.patch"
 patch -p1 < "${FILESDIR}/android-misc.patch"
 patch -p1 < "${FILESDIR}/check-crypt.patch"
-patch -p1 < "${BASE}/src/Port-Python-s-SSL-module-to-OpenSSL-1.1.0-4.patch"
+patch -p1 < "${BASE}/src/Port-Python-s-SSL-module-to-OpenSSL-1.1.0-5.patch"
 
 rm -rf Modules/_ctypes/{darwin,libffi}*
 
