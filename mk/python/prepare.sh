fa0326098fd15d879a7734a0d4a881172582b676
Yen Chi Hsuan <yan12125@gmail.com>
2016-09-12 04:36:51 +0800

    python: drop non-build-time patches

--- mk/python/prepare.sh
+++ mk/python/prepare.sh
@@ -1,5 +1,3 @@
-patch -F0 -p1 -i "${FILESDIR}/python-misc.patch"
-patch -F0 -p1 -i "${FILESDIR}/android-misc.patch"
 patch -F0 -p1 -i "${FILESDIR}/check-crypt.patch"
 
 rm -rf Modules/_ctypes/{darwin,libffi}*
