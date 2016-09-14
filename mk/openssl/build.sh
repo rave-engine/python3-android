50470e1bb7c98d48bd0de3474e1f972f1231c493
Yen Chi Hsuan <yan12125@gmail.com>
2016-09-15 00:56:59 +0800

    Add x86_64

--- mk/openssl/build.sh
+++ mk/openssl/build.sh
@@ -2,6 +2,7 @@ case "$ANDROID_PLATFORM" in
     arm)    openssl_target=android-armeabi;;
     arm64)  openssl_target=android64-aarch64;;
     x86)    openssl_target=android-x86;;
+    x86_64) openssl_target=android64-x86_64;;
     mips)   openssl_target=android-mips;;
     mips64) openssl_target=android64-mips64;;
 esac
