0fba183644d90ea64cfb9a37f1741ec20ea64379
Yen Chi Hsuan <yan12125@gmail.com>
2016-11-19 00:15:46 +0800

    Add /usr/ to env.sh

--- devscripts/env.sh
+++ devscripts/env.sh
@@ -1,10 +1,10 @@
 PYTHON3_ROOT=/data/local/tmp/python3
 
-export PATH=$PATH:$PYTHON3_ROOT/bin
+export PATH=$PATH:$PYTHON3_ROOT/usr/bin
 if [ ! -z "$LD_LIBRARY_PATH" ] ; then
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:"
 fi
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH$PYTHON3_ROOT/lib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH$PYTHON3_ROOT/usr/lib"
 export SSL_CERT_DIR=$PYTHON3_ROOT/share/certs
 # For ncurses
 export TERMINFO=$PYTHON3_ROOT/share/terminfo
