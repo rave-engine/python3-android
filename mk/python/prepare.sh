c2d4b4fd33d0a55a095208fcab2566f1a5287977
Yen Chi Hsuan <yan12125@gmail.com>
2016-11-22 11:57:43 +0800

    python: Force UTF-8 everywhere
    This reduces startup time by 40%
    
    See: http://bugs.python.org/issue28596

--- mk/python/prepare.sh
+++ mk/python/prepare.sh
@@ -1,4 +1,5 @@
 patch -F0 -p1 -i "${FILESDIR}/check-crypt.patch"
 patch -F0 -p1 -i "${FILESDIR}/ncurses-headers.patch"
+patch -F0 -p1 -i "${FILESDIR}/android-locale-utf8.patch"
 
 mkdir build-target
