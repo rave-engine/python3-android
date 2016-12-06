6cbbed29bd4d63d0819df3d168c69ab6ca755da9
Yen Chi Hsuan <yan12125@gmail.com>
2016-12-07 01:07:05 +0800

    python: Fix compilation of C modules
    On Debian/Ubuntu, dpkg-configure reports multiarch triplet for host
    builds, which leads to incorrect include paths and thus build failures
    of C modules in stdlib

--- mk/python/prepare.sh
+++ mk/python/prepare.sh
@@ -3,5 +3,6 @@ patch -F0 -p1 -i "${FILESDIR}/gdbm.patch"
 patch -F0 -p1 -i "${FILESDIR}/decimal.patch"
 patch -F0 -p1 -i "${FILESDIR}/ncurses-headers.patch"
 patch -F0 -p1 -i "${FILESDIR}/android-locale-utf8.patch"
+patch -F0 -p1 -i "${FILESDIR}/fix-cross-modules.patch"
 
 mkdir build-target
