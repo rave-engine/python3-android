c4fc25b0841fa84f758b6bab8ed93fdcb209c8c1
Yen Chi Hsuan <yan12125@gmail.com>
2016-12-08 02:06:04 +0800

    python: Use patches on bugs.python.org

--- mk/python/prepare.sh
+++ mk/python/prepare.sh
@@ -1,8 +1,8 @@
 patch -F0 -p1 -i "${FILESDIR}/check-crypt.patch"
 patch -F0 -p1 -i "${FILESDIR}/gdbm.patch"
 patch -F0 -p1 -i "${FILESDIR}/decimal.patch"
-patch -F0 -p1 -i "${FILESDIR}/ncurses-headers.patch"
-patch -F0 -p1 -i "${FILESDIR}/android-locale-utf8.patch"
+patch -F0 -p1 -i "${BASE}/src/ncurses-headers.patch"
+patch -F0 -p1 -i "${BASE}/src/android-locale-utf8.patch"
 patch -F0 -p1 -i "${BASE}/src/0001-setup.py-do-not-add-invalid-header-locations.patch"
 
 mkdir build-target
