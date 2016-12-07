df0073845da0d43795bf4161f0178e4890ec792d
Yen Chi Hsuan <yan12125@gmail.com>
2016-12-08 01:16:44 +0800

    python: Fix cross-compile modules (again)
    And drop wrong patches

--- mk/python/prepare.sh
+++ mk/python/prepare.sh
@@ -3,6 +3,6 @@ patch -F0 -p1 -i "${FILESDIR}/gdbm.patch"
 patch -F0 -p1 -i "${FILESDIR}/decimal.patch"
 patch -F0 -p1 -i "${FILESDIR}/ncurses-headers.patch"
 patch -F0 -p1 -i "${FILESDIR}/android-locale-utf8.patch"
-patch -F0 -p1 -i "${FILESDIR}/fix-cross-modules.patch"
+patch -F0 -p1 -i "${BASE}/src/0001-setup.py-do-not-add-invalid-header-locations.patch"
 
 mkdir build-target
