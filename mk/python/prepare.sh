32585d4601c11ef708258ea809e6b5532860373a
Yen Chi Hsuan <yan12125@gmail.com>
2016-12-17 17:34:46 +0800

    python: UTF-8 patched upstreamed
    https://hg.python.org/cpython/rev/1756beed417c
    http://bugs.python.org/28596

--- mk/python/prepare.sh
+++ mk/python/prepare.sh
@@ -2,6 +2,5 @@ patch -F0 -p1 -i "${FILESDIR}/check-crypt.patch"
 patch -F0 -p1 -i "${FILESDIR}/gdbm.patch"
 patch -F0 -p1 -i "${FILESDIR}/decimal.patch"
 patch -F0 -p1 -i "${FILESDIR}/ncurses-headers.patch"
-patch -F0 -p1 -i "${BASE}/src/android-locale-utf8.patch"
 
 mkdir build-target
