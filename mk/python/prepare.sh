fe5e802ea4d0b9c9f29d66ee8804d0fcf628c683
Yen Chi Hsuan <yan12125@gmail.com>
2016-12-17 23:41:45 +0800

    python: Hack distutils to include --sysroot flags

--- mk/python/prepare.sh
+++ mk/python/prepare.sh
@@ -2,5 +2,6 @@ patch -F0 -p1 -i "${FILESDIR}/check-crypt.patch"
 patch -F0 -p1 -i "${FILESDIR}/gdbm.patch"
 patch -F0 -p1 -i "${FILESDIR}/decimal.patch"
 patch -F0 -p1 -i "${FILESDIR}/ncurses-headers.patch"
+patch -F0 -p1 -i "${FILESDIR}/distutils-android-sysroot.patch"
 
 mkdir build-target
