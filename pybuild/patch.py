b8a4a490ac287e21c2d76317f127474fc1a94097
Yen Chi Hsuan <yan12125@gmail.com>
2017-01-03 18:17:08 +0800

    pybuild: Improved patch handling
    - Customizable --strip
    - Allow files without .patch

--- pybuild/patch.py
+++ pybuild/patch.py
@@ -3,20 +3,24 @@ from .util import BASE
 
 
 class Patch:
-    def __init__(self, source: Source, name: str):
+    def __init__(self, source: Source, name: str, strip=1):
         self.source = source
         self.name = name
+        self.strip = strip
 
     def apply_file(self, patch):
+        patch_path = str(patch)
+        if not patch.exists():
+            patch_path += '.patch'
         self.source.run_in_source_dir([
-            'patch', '--fuzz=0', '--strip=1', '--input', patch])
+            'patch', '--fuzz=0', f'--strip={self.strip}', '--input', patch_path])
 
 
 class LocalPatch(Patch):
     def apply(self):
-        self.apply_file(self.source.package.filesdir / f'{self.name}.patch')
+        self.apply_file(self.source.package.filesdir / self.name)
 
 
 class RemotePatch(Patch):
     def apply(self):
-        self.apply_file(BASE / Source.src_prefix / f'{self.name}.patch')
+        self.apply_file(BASE / Source.src_prefix / self.name)
