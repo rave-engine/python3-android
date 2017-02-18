65e8dc318fe081cda75c45a1b6c1601aedd84c9b
Yen Chi Hsuan <yan12125@gmail.com>
2017-02-18 22:43:42 +0800

    env.sh: set $HOME to a writable directory
    python's test_site needs writable $HOME

--- devscripts/env.sh
+++ devscripts/env.sh
@@ -8,3 +8,5 @@ export LD_LIBRARY_PATH="$LD_LIBRARY_PATH$PYTHON3_ROOT/usr/lib"
 export SSL_CERT_DIR=$PYTHON3_ROOT/etc/ssl/certs
 # For ncurses
 export TERMINFO=$PYTHON3_ROOT/usr/share/terminfo
+
+export HOME=/data/local/tmp
