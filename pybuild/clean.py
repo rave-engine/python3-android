abeeacdb304de0c0b3b54691a09cbd46d9814423
Yen Chi Hsuan <yan12125@gmail.com>
2017-05-14 20:31:37 +0800

    pybuild: Add an option to clean a single package

--- pybuild/clean.py
+++ pybuild/clean.py
@@ -1,10 +1,26 @@
+import argparse
+
 from .package import Package
-from .package import enumerate_packages
+from .package import enumerate_packages, import_package
 from .util import rmtree
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Clean some packages')
+    parser.add_argument(
+        '--package',
+        help='Package name. All packages will be cleaned if not provided.')
+
+    return parser.parse_args()
+
+
 def main():
-    for pkg in enumerate_packages():
+    args = parse_args()
+    if args.package:
+        target_packages = [import_package(args.package)]
+    else:
+        target_packages = enumerate_packages()
+    for pkg in target_packages:
         for src in pkg.sources:
             src.clean()
 
