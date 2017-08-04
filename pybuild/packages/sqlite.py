20b7ffda14a1cf15a349c6498f49a543884a32b8
Yen Chi Hsuan <yan12125@gmail.com>
2017-08-04 11:14:50 +0800

    sqlite: Bump to 3.20.0

--- pybuild/packages/sqlite.py
+++ pybuild/packages/sqlite.py
@@ -4,7 +4,7 @@ from ..util import target_arch
 
 
 class SQLite(Package):
-    source = URLSource('https://www.sqlite.org/2017/sqlite-autoconf-3190300.tar.gz')
+    source = URLSource('https://www.sqlite.org/2017/sqlite-autoconf-3200000.tar.gz')
 
     def prepare(self):
         self.run_with_env([
