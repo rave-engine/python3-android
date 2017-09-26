8809c65976b639864f42b36a5f38f7e9a6a5e469
Yen Chi Hsuan <yan12125@gmail.com>
2017-09-27 00:53:53 +0800

    readline: Go back to 7.0.003
    I was using git repo as downloading tarballs and patches takes quite a
    few time. With the new Bintray-based cache mechanism, there's no need to
    download files anymore to get a built readline directory

--- pybuild/packages/readline.py
+++ pybuild/packages/readline.py
@@ -1,12 +1,32 @@
-from ..source import GitSource
+from ..source import URLSource
 from ..package import Package
+from ..patch import RemotePatch
 from ..util import target_arch
 
 
 class Readline(Package):
-    source = GitSource('https://git.savannah.gnu.org/git/readline.git',
-                       alias='readline', branch='devel')
+    _MAJOR = 7
+    _MINOR = 0
+    _PATCHLEVEL = 3
+    version = f'{_MAJOR}.{_MINOR}.{_PATCHLEVEL:03d}'
+
+    _common = f'https://ftp.gnu.org/gnu/readline/readline-{_MAJOR}.{_MINOR}'
+    source = URLSource(f'{_common}.tar.gz')
     dependencies = ['ncurses']
+
+    # Use property as list comprehension creates a new scope not mixing with
+    # the class scope
+    _patches = None
+
+    @property
+    def patches(self):
+        if self._patches is None:
+            self._patches = [
+                RemotePatch(
+                    f'{self._common}-patches/readline{self._MAJOR}{self._MINOR}-{patch:03d}',
+                    strip=0)
+                for patch in range(1, self._PATCHLEVEL + 1)]
+        return self._patches
 
     def prepare(self):
         # See the wcwidth() test in aclocal.m4. Tested on Android 6.0 and it's broken
