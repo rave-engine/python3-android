d000384993184ea385ffc7b753e9b4b582e07d9a
Chih-Hsuan Yen <yan12125@gmail.com>
2018-01-04 20:30:20 +0800

    gdbm: Bump to 1.14.1
    soname bug :)
    
    https://bugs.python.org/issue32484

--- pybuild/packages/gdbm.py
+++ pybuild/packages/gdbm.py
@@ -4,7 +4,7 @@ from ..util import target_arch
 
 
 class GDBM(Package):
-    version = '1.14'
+    version = '1.14.1'
     source = URLSource(f'https://ftp.gnu.org/gnu/gdbm/gdbm-{version}.tar.gz')
 
     def prepare(self):
