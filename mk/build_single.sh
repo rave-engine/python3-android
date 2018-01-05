4d61bda1955750755ce801f9f9129197ed1a5134
Brian Spratke <spratke@gmail.com>
2018-01-05 11:06:26 -0500

    Updates to get compile
    These updates seem to have me compiling. Working on linking

--- mk/build_single.sh
+++ mk/build_single.sh
@@ -3,91 +3,65 @@ source ./env
 [[ ! -d "${ANDROID_PREFIX}/${BUILD_IDENTIFIER}" ]] && (mkdir -p "${ANDROID_PREFIX}/${BUILD_IDENTIFIER}" || exit 1)
 [[ ! -d "${ANDROID_PREFIX}/${BUILD_IDENTIFIER}/include" ]] && (mkdir "${ANDROID_PREFIX}/${BUILD_IDENTIFIER}/include" || exit 1)
 
-export PATH="${ANDROID_TOOL_PREFIX}/${BUILD_IDENTIFIER}/bin:${PATH}"
+
+#export PATH="${ANDROID_TOOL_PREFIX}/${BUILD_IDENTIFIER}/bin:${PATH}"
 export PREFIX="${ANDROID_PREFIX}/${BUILD_IDENTIFIER}"
 export TOOL_PREFIX="${ANDROID_TOOL_PREFIX}/${BUILD_IDENTIFIER}"
 export HOST="${ANDROID_HOST}"
 export TARGET="${ANDROID_TARGET}"
 
 export NDK_ROOT="${BASE}/sdk/${NDK_REL}"
-export SDK_ROOT="${BASE}/sdk/${SDK_REL}"
-export NDK_PLATFORM="android-${NDK_REV}"
-export SDK_PLATFORM="android-${SDK_REV}"
-export cross="${ANDROID_TARGET}-"
 
-# BDS NEW
-CLANG_PREFIX="${NDK_ROOT}/toolchains/llvm/prebuilt/linux-x86_64"
-export SYSROOT="${BASE}/build-tools/${BUILD_IDENTIFIER}" 
-##SYSROOT_INCLUDE="${NDK_ROOT}/sysroot"
-
-
-
-case "${NDK_REV}" in
-  16*)
-	   export NDK_SYS_ROOT="${NDK_ROOT}/sysroot"
-	   ;;
-	*)
-	   export NDK_SYS_ROOT="${TOOL_PREFIX}/sysroot"
-	   ;;
-esac	   
-
-##export NEW_SYSROOT="${NDK_ROOT}/platforms/android-${ANDROID_API_LEVEL}/arch-x86_64/usr"
-#export NEW_SYSROOT="${NDK_ROOT}/platforms/android-${ANDROID_API_LEVEL}/arch-${ANDROID_PLATFORM}/usr"
-export NEW_SYSROOT="${NDK_ROOT}/sysroot/usr"
-
-
-
-# case "${NDK_REV}" in
-#   15*|16*)
-#       #export CFLAGS="--sysroot ${NDK_SYS_ROOT} -I${TOOL_PREFIX}/include -DANDROID -mandroid ${CFLAGS_EXTRA} -isystem ${TOOL_PREFIX}/sysroot/usr/include/${ANDROID_TARGET} -D__ANDROID_API__=${ANDROID_API_LEVEL}" 
-#       export CFLAGS="--sysroot ${NDK_SYS_ROOT} -I${TOOL_PREFIX}/include -DANDROID ${CFLAGS_EXTRA} -isystem ${TOOL_PREFIX}/sysroot/usr/include/${ANDROID_TARGET} -D__ANDROID_API__=${ANDROID_API_LEVEL}" 
-#       ;;
-#   *)
-# 	  export CFLAGS="--sysroot ${TOOL_PREFIX}/sysroot -I${PREFIX}/include -I${TOOL_PREFIX}/include -DANDROID -mandroid ${CFLAGS_EXTRA}" 
-#       ;;
-# esac
-
-
+export TOOL_PREFIX="${NDK_ROOT}/toolchains/${ANDROID_TOOLCHAIN}/prebuilt/linux-x86_64"
+export CLANG_PREFIX="${NDK_ROOT}/toolchains/llvm/prebuilt/linux-x86_64"
 export LLVM_BASE_FLAGS="-target ${LLVM_TARGET} -gcc-toolchain ${TOOL_PREFIX}"
+export ARCH_SYSROOT="${NDK_ROOT}/platforms/android-${ANDROID_API_LEVEL}/arch-${ANDROID_PLATFORM}/usr"
+export UNIFIED_SYSROOT="${NDK_ROOT}/sysroot/usr"
 
-
-##export CPPFLAGS="${LLVM_BASE_FLAGS} --sysroot ${NEW_SYSROOT} -I${SYSROOT_INCLUDE}/usr/include/${ANDROID_TARGET} -I${PREFIX}/include -DANDROID ${CPPFLAGS_EXTRA}"
-export CPPFLAGS="${LLVM_BASE_FLAGS} --sysroot ${NEW_SYSROOT} -I${NEW_SYSROOT}/include -I${NEW_SYSROOT}/include/arm-linux-androideabi -I${PREFIX}/include -DANDROID ${CPPFLAGS_EXTRA} -D__ANDROID_API__=${ANDROID_API_LEVEL}"
-#export CXXFLAGS="--sysroot ${SYSROOT}"
-##export CFLAGS="${LLVM_BASE_FLAGS} -I${SYSROOT_INCLUDE}/usr/include -I${SYSROOT_INCLUDE}/usr/include/${ANDROID_TARGET} -D__ANDROID_API__=${ANDROID_API_LEVEL} -Werror=implicit-function-declaration"
-export CFLAGS="${LLVM_BASE_FLAGS}  -I${NEW_SYSROOT}/include -I${NEW_SYSROOT}/include/arm-linux-androideabi -Werror=implicit-function-declaration"
-if [ "$ANDROID_API_LEVEL" -ge 21 ] ; then	
-    export CFLAGS="$CFLAGS -fPIE"
-fi
-export CFLAGS="${CFLAGS} ${CPPFLAGS_EXTRA}"
-export CXXFLAGS="${CXXFLAGS} ${CXXFLAGS_EXTRA}"
-export LDFLAGS="${LLVM_BASE_FLAGS} --sysroot ${NEW_SYSROOT} -L${PREFIX}/lib ${LDFLAGS_EXTRA}"
-#export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS_EXTRA}"
-
-echo "SYSROOT_INCLUDE IS ${NEW_SYSROOT}"
-#echo "Hello World LDFlags=${LDFLAGS}" 
-
-# export CPPFLAGS="${CFLAGS} ${CPPFLAGS_EXTRA}"
-# export CXXFLAGS="${CFLAGS} ${CXXFLAGS_EXTRA}"
-# export LDFLAGS="--sysroot ${TOOL_PREFIX}/sysroot -L${PREFIX}/lib -L${TOOL_PREFIX}/lib ${LDFLAGS_EXTRA}"
-
-# export CC="${ANDROID_TARGET}-gcc"
-# export CXX="${ANDROID_TARGET}-g++"
-# export CPP="${ANDROID_TARGET}-cpp"
 export CC="${CLANG_PREFIX}/bin/clang"
 export CXX="${CLANG_PREFIX}/bin/clang++"
-export CPP="${CLANG_PREFIX}/bin/clang++"
+export CPP="${CLANG_PREFIX}/bin/clang"
+
+export CPPFLAGS="${LLVM_BASE_FLAGS} --sysroot=${ARCH_SYSROOT} -isystem ${UNIFIED_SYSROOT}/include -isystem ${UNIFIED_SYSROOT}/include/${ANDROID_TARGET} -D__ANDROID_API__=${ANDROID_API_LEVEL}"
+
+export CFLAGS="-fPIC"
+
+case "${ANDROID_PLATFORM}" in
+  arm)
+      export CFLAGS="${CFLAGS} -fno-integrated-as" 
+      ;;
+esac
+
+
+export CXXFLAGS="${CFLAGS}"
+export LDFLAGS="${LLVM_BASE_FLAGS} --sysroot=${ARCH_SYSROOT} -pie"
+
+
+#?????
+# export CPPFLAGS="$CPPFLAGS -${CPPFLAGS_EXTRA}"
+# export LDFLAGS="$LDFLAGS ${"
+
+
+# self.env['CPPFLAGS'].extend(['-I', f'{dep_pkg.destdir()}/usr/include'])
+# self.env['LDFLAGS'].extend(['-L', f'{dep_pkg.destdir()}/usr/lib'])
+
+
+
+export AR="${TOOL_PREFIX}/bin/${ANDROID_TARGET}-ar"
+export AS="${TOOL_PREFIX}/bin/${ANDROID_TARGET}-as"
+export LD="${TOOL_PREFIX}/bin/${ANDROID_TARGET}-ld"
+export OBJCOPY="${TOOL_PREFIX}/bin/${ANDROID_TARGET}-objcopy"
+export OBJDUMP="${TOOL_PREFIX}/bin/${ANDROID_TARGET}-objdump"
+export RANLIB="${TOOL_PREFIX}/bin/${ANDROID_TARGET}-ranlib"
+export STRIP="${TOOL_PREFIX}/bin/${ANDROID_TARGET}-strip"
+export READELF="${TOOL_PREFIX}/bin/${ANDROID_TARGET}-readelf"
+
+export PATH="${TOOL_PREFIX}/${ANDROID_TARGET}/bin:${PATH}"
+
 
 chmod +x "${CC}" "${CXX}" "${CPP}"
+#chmod -R +x * "${NDK_ROOT}"
 
-
-export AR="${ANDROID_TARGET}-ar"
-export AS="${ANDROID_TARGET}-ls"
-export LD="${ANDROID_TARGET}-ld"
-export OBJCOPY="${ANDROID_TARGET}-objcopy"
-export OBJDUMP="${ANDROID_TARGET}-objdump"
-export RANLIB="${ANDROID_TARGET}-ranlib"
-export STRIP="${ANDROID_TARGET}-strip"
 
 export NAME="$1"
 export VERSION="$2"
