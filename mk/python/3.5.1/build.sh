b6e761785bb1970783f148e49c0374281322ebd2
Brian Spratke <spratke@gmail.com>
2018-01-26 10:57:43 -0500

    x64 building
    At this point x32 is not building, but x64 is.  Needs more work.

--- mk/python/3.5.1/build.sh
+++ mk/python/3.5.1/build.sh
@@ -5,9 +5,21 @@ tar -xf "Python-${VERSION}.tar.xz" || exit 1
 pushd "Python-${VERSION}" >/dev/null
 
 # Build host components.
-AR=ar AS=as CC=gcc CFLAGS= CPP=cpp CPPFLAGS= CXX=g++ CXXFLAGS= LD=ld LDFLAGS= RANLIB=ranlib ./configure || exit 1
-AR=ar AS=as CC=gcc CFLAGS= CPP=cpp CPPFLAGS= CXX=g++ CXXFLAGS= LD=ld LDFLAGS= RANLIB=ranlib make BUILDPYTHON=hostpython hostpython PGEN=Parser/hostpgen Parser/hostpgen || exit 1
-make distclean || exit 1
+#AR=ar AS=as CC=gcc CFLAGS= CPP=cpp CPPFLAGS= CXX=g++ CXXFLAGS= LD=ld LDFLAGS= RANLIB=ranlib ./configure || exit 1
+#AR=ar AS=as CC=gcc CFLAGS= CPP=cpp CPPFLAGS= CXX=g++ CXXFLAGS= LD=ld LDFLAGS= RANLIB=ranlib make BUILDPYTHON=hostpython hostpython PGEN=Parser/hostpgen Parser/hostpgen || exit 1
+
+# echo "here are som values prefix=${PREFIX} host=${TARGET} build=${HOST} "
+# sleep 15
+
+#echo "Step 1"
+#AR=ar AS=as CC=clang CFLAGS= CPP="clang -E" CPPFLAGS= CXX=clang++ CXXFLAGS= LD=ld LDFLAGS= RANLIB=ranlib ./configure || exit 1
+#echo "Step 2"
+#AR=ar AS=as CC=clang CFLAGS= CPP="clang -E" CPPFLAGS= CXX=clang++ CXXFLAGS= LD=ld LDFLAGS= RANLIB=ranlib make BUILDPYTHON=hostpython hostpython PGEN=Parser/hostpgen Parser/hostpgen || exit 1
+
+echo "Step 3"
+#make distclean || exit 1
+
+echo "Step 4"
 
 # Apply patches and build target Python.
 cat > config.site <<-SITE
@@ -25,7 +37,27 @@ patch -p1  < "${FILESDIR}/${PACKAGE}-android-extras.patch" || exit 1
 #patch -p1  < "${FILESDIR}/${PACKAGE}-accept4.patch" || exit 1
 patch -p1  < "${FILESDIR}/${PACKAGE}-python-nl_langinfo.patch" || exit 1 
 
-./configure CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" CONFIG_SITE=config.site --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --disable-ipv6 --enable-shared --without-ensurepip || exit 1
+
+echo "Step 5"
+sleep 1m
+
+./configure CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" CONFIG_SITE=config.site --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --disable-ipv6 --enable-shared --without-ensurepip || exit 1 
+
+# ./configure CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" CONFIG_SITE=config.site \
+#     --prefix="${PREFIX}" \
+#     --host="${HOST}" \
+#     --build="${HOST}" \
+#     --disable-ipv6 \
+#     --target="${TARGET}"
+#     #--enable-shared \
+#     # --prefix=/usr \
+#     # --host="${ANDROID_TARGET}" \
+#     # --build=x86_64-linux-gnu \
+#     # --with-system-ffi \
+#     # --with-system-expat \
+#     --without-ensurepip || exit 1
+
+
 make CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" HOSTPGEN="$(pwd)/Parser/hostpgen" || exit 1
 make CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" HOSTPGEN="$(pwd)/Parser/hostpgen" install || exit 1
 
