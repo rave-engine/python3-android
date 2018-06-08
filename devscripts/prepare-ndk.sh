a15f523a254b1983ba50279afa8a21a2d7f594cf
Chih-Hsuan Yen <yan12125@gmail.com>
2018-06-08 13:33:19 +0800

    Remove existing NDKs before extracting

--- devscripts/prepare-ndk.sh
+++ devscripts/prepare-ndk.sh
@@ -7,9 +7,12 @@ NDK_VER=r17
 HOST_OS="$(uname | tr 'A-Z' 'a-z')"
 NDK_ARCHIVE=android-ndk-$NDK_VER-$HOST_OS-x86_64.zip
 wget --no-verbose https://dl.google.com/android/repository/$NDK_ARCHIVE
+
+rm -rf $(pwd)/android-ndk-$NDK_VER
 unzip -q $NDK_ARCHIVE
 
-mv -f $(pwd)/android-ndk-$NDK_VER $(pwd)/android-ndk
+rm -rf $(pwd)/android-ndk
+mv $(pwd)/android-ndk-$NDK_VER $(pwd)/android-ndk
 
 "$SCRIPTPATH"/strip-ndk.sh $(pwd)/android-ndk
 
