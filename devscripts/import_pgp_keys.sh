158eff3d5149148e323e2f1fb3642c97d983b69b
Chih-Hsuan Yen <yan12125@gmail.com>
2018-06-23 03:40:05 +0800

    Cache PGP keys in the repo
    Mimic archlinux-keyring :)

--- devscripts/import_pgp_keys.sh
+++ devscripts/import_pgp_keys.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gpg --debug ipc --keyserver hkp://ipv4.pool.sks-keyservers.net --recv-keys $(python3.8 -m pybuild.pgp_keys)
+cat devscripts/pgp-keys/*.asc | gpg --import
--- pybuild/pgp_keys.py
+++ pybuild/pgp_keys.py
@@ -1,14 +1,46 @@
+import itertools
+import logging
+import tempfile
+
+import gnupg
+
 from .env import gpg_key_id
 from .package import enumerate_packages, import_package
+from .util import BASE
+
+GPG_SERVER = 'pool.sks-keyservers.net'
 
 
 def main():
-    target_packages = enumerate_packages()
-    for pkgname in target_packages:
-        pkg = import_package(pkgname)
-        if hasattr(pkg, 'validpgpkeys'):
-            print('\n'.join(pkg.validpgpkeys))
-    print(gpg_key_id)
+    logging.basicConfig(level=logging.DEBUG)
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        gpg = gnupg.GPG(gnupghome=tmpdir)
+        gpg.encoding = 'utf-8'
+
+        keys_map = {}
+
+        target_packages = enumerate_packages()
+        for pkgname in target_packages:
+            pkg = import_package(pkgname)
+            keys_map[pkgname] = getattr(pkg, 'validpgpkeys', [])
+
+        # Also fetch my signing key for prebuilt archives
+        keys_map['yan12125'] = [gpg_key_id]
+
+        result = gpg.recv_keys(GPG_SERVER, *list(itertools.chain.from_iterable(keys_map.values())))
+        assert result
+
+        pgp_keys_dir = BASE / 'devscripts' / 'pgp-keys'
+        for name, key_ids in keys_map.items():
+            exported_keys = ''
+            for key in key_ids:
+                current_key = gpg.export_keys([key], armor=True)
+                exported_keys += current_key
+
+            if exported_keys:
+                with open(pgp_keys_dir / (name + '.asc'), 'w') as f:
+                    f.write(exported_keys)
 
 
 if __name__ == '__main__':
