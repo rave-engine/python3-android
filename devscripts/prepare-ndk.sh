3118dc8b6f93ecdfade4b78782c3fced03b54c2f
Chih-Hsuan Yen <yan12125@gmail.com>
2018-07-28 18:50:56 +0800

    NDK r18 beta 1

--- devscripts/prepare-ndk.sh
+++ devscripts/prepare-ndk.sh
@@ -3,7 +3,7 @@
 # https://stackoverflow.com/a/4774063
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-NDK_VER=r17b
+NDK_VER=r18-beta1
 HOST_OS="$(uname | tr 'A-Z' 'a-z')"
 NDK_ARCHIVE=android-ndk-$NDK_VER-$HOST_OS-x86_64.zip
 wget --no-verbose https://dl.google.com/android/repository/$NDK_ARCHIVE
