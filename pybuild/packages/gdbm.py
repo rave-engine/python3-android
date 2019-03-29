b0b5fab2c9145dc4f6579c90e22826940edfa07b
Chih-Hsuan Yen <yan12125@gmail.com>
2019-03-29 22:32:26 +0800

    gdbm: eliminate SIZE_T_MAX warnings

--- pybuild/packages/gdbm.py
+++ pybuild/packages/gdbm.py
@@ -1,5 +1,6 @@
 from ..source import URLSource
 from ..package import Package
+from ..patch import LocalPatch
 from ..util import target_arch
 
 
@@ -7,6 +8,9 @@ class GDBM(Package):
     version = '1.18.1'
     source = URLSource(f'https://ftp.gnu.org/gnu/gdbm/gdbm-{version}.tar.gz', sig_suffix='.sig')
     validpgpkeys = ['325F650C4C2B6AD58807327A3602B07F55D0C732']
+    patches = [
+        LocalPatch('SIZE_T_MAX'),
+    ]
 
     def prepare(self):
         self.run_with_env([
