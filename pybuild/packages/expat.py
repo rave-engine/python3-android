0b81f815371efc7bc25af67723ce9aa708d7d7b8
Chih-Hsuan Yen <yan12125@gmail.com>
2019-08-19 22:01:21 +0800

    expat: avoid unknown warning options

--- pybuild/packages/expat.py
+++ pybuild/packages/expat.py
@@ -1,5 +1,6 @@
 from ..source import URLSource
 from ..package import Package
+from ..patch import LocalPatch
 from ..util import target_arch
 
 
@@ -7,8 +8,13 @@ class Expat(Package):
     version = '2.2.7'
     source = URLSource(f'https://github.com/libexpat/libexpat/releases/download/R_{version.replace(".", "_")}/expat-{version}.tar.bz2', sig_suffix='.asc')
     validpgpkeys = ['3D7E959D89FACFEE38371921B00BC66A401A1600']
+    patches = [
+        LocalPatch('avoid-unknown-warning-options'),
+    ]
 
     def prepare(self):
+        self.run(['autoreconf', '--install', '--verbose', '--force'])
+
         self.run_with_env([
             './configure',
             '--prefix=/usr',
